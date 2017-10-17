4717b6beebeb37ac02dfcc8b6d18178f70c5c626
Ivan <i.vaigult@gmail.com>
2017-10-17 12:03:57 +0300

    Add cookie parameter

--- glespp/reflection.hpp
+++ glespp/reflection.hpp
@@ -13,27 +13,27 @@
 #define PP_ARG_LEN(...) __PP_EVAL(__PP_ARG_LEN(__VA_ARGS__, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0))
 #define __PP_ARG_LEN(x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, num_args, ...) num_args
 
-#define __PP_FOR_EACH_0(fn, ...)
-#define __PP_FOR_EACH_1(fn, x, ...)  fn(x) __PP_EVAL(__PP_FOR_EACH_0(fn, __VA_ARGS__))
-#define __PP_FOR_EACH_2(fn, x, ...)  fn(x) __PP_EVAL(__PP_FOR_EACH_1(fn, __VA_ARGS__))
-#define __PP_FOR_EACH_3(fn, x, ...)  fn(x) __PP_EVAL(__PP_FOR_EACH_2(fn, __VA_ARGS__))
-#define __PP_FOR_EACH_4(fn, x, ...)  fn(x) __PP_EVAL(__PP_FOR_EACH_3(fn, __VA_ARGS__))
-#define __PP_FOR_EACH_5(fn, x, ...)  fn(x) __PP_EVAL(__PP_FOR_EACH_4(fn, __VA_ARGS__))
-#define __PP_FOR_EACH_6(fn, x, ...)  fn(x) __PP_EVAL(__PP_FOR_EACH_5(fn, __VA_ARGS__))
-#define __PP_FOR_EACH_7(fn, x, ...)  fn(x) __PP_EVAL(__PP_FOR_EACH_6(fn, __VA_ARGS__))
-#define __PP_FOR_EACH_8(fn, x, ...)  fn(x) __PP_EVAL(__PP_FOR_EACH_7(fn, __VA_ARGS__))
-#define __PP_FOR_EACH_9(fn, x, ...)  fn(x) __PP_EVAL(__PP_FOR_EACH_8(fn, __VA_ARGS__))
-#define __PP_FOR_EACH_10(fn, x, ...) fn(x) __PP_EVAL(__PP_FOR_EACH_9(fn, __VA_ARGS__))
-#define __PP_FOR_EACH_11(fn, x, ...) fn(x) __PP_EVAL(__PP_FOR_EACH_10(fn, __VA_ARGS__))
-#define __PP_FOR_EACH_12(fn, x, ...) fn(x) __PP_EVAL(__PP_FOR_EACH_11(fn, __VA_ARGS__))
+#define __PP_FOR_EACH_0(fn,  cookie,    ...)
+#define __PP_FOR_EACH_1(fn,  cookie, x, ...) fn(cookie, x) __PP_EVAL(__PP_FOR_EACH_0( fn, cookie, __VA_ARGS__))
+#define __PP_FOR_EACH_2(fn,  cookie, x, ...) fn(cookie, x) __PP_EVAL(__PP_FOR_EACH_1( fn, cookie, __VA_ARGS__))
+#define __PP_FOR_EACH_3(fn,  cookie, x, ...) fn(cookie, x) __PP_EVAL(__PP_FOR_EACH_2( fn, cookie, __VA_ARGS__))
+#define __PP_FOR_EACH_4(fn,  cookie, x, ...) fn(cookie, x) __PP_EVAL(__PP_FOR_EACH_3( fn, cookie, __VA_ARGS__))
+#define __PP_FOR_EACH_5(fn,  cookie, x, ...) fn(cookie, x) __PP_EVAL(__PP_FOR_EACH_4( fn, cookie, __VA_ARGS__))
+#define __PP_FOR_EACH_6(fn,  cookie, x, ...) fn(cookie, x) __PP_EVAL(__PP_FOR_EACH_5( fn, cookie, __VA_ARGS__))
+#define __PP_FOR_EACH_7(fn,  cookie, x, ...) fn(cookie, x) __PP_EVAL(__PP_FOR_EACH_6( fn, cookie, __VA_ARGS__))
+#define __PP_FOR_EACH_8(fn,  cookie, x, ...) fn(cookie, x) __PP_EVAL(__PP_FOR_EACH_7( fn, cookie, __VA_ARGS__))
+#define __PP_FOR_EACH_9(fn,  cookie, x, ...) fn(cookie, x) __PP_EVAL(__PP_FOR_EACH_8( fn, cookie, __VA_ARGS__))
+#define __PP_FOR_EACH_10(fn, cookie, x, ...) fn(cookie, x) __PP_EVAL(__PP_FOR_EACH_9( fn, cookie, __VA_ARGS__))
+#define __PP_FOR_EACH_11(fn, cookie, x, ...) fn(cookie, x) __PP_EVAL(__PP_FOR_EACH_10(fn, cookie, __VA_ARGS__))
+#define __PP_FOR_EACH_12(fn, cookie, x, ...) fn(cookie, x) __PP_EVAL(__PP_FOR_EACH_11(fn, cookie, __VA_ARGS__))
 
-#define PP_FOR_EACH(fn, ...) __PP_FOR_EACH(PP_ARG_LEN(__VA_ARGS__), fn, ##__VA_ARGS__)
-#define __PP_FOR_EACH(n, fn, ...) __PP_EVAL(__PP_CAT(__PP_FOR_EACH_, n) (fn, ##__VA_ARGS__))
+#define PP_FOR_EACH(fn, cookie, ...) __PP_FOR_EACH(PP_ARG_LEN(__VA_ARGS__), fn, cookie, ##__VA_ARGS__)
+#define __PP_FOR_EACH(n, fn, cookie, ...) __PP_EVAL(__PP_CAT(__PP_FOR_EACH_, n) (fn, cookie, ##__VA_ARGS__))
 
-#define PP_IMPL_VISIT(type_field_pair) __PP_EVAL(__PP_IMPL_VISIT type_field_pair)
+#define PP_IMPL_VISIT(cookie, type_field_pair) __PP_EVAL(__PP_IMPL_VISIT type_field_pair)
 #define __PP_IMPL_VISIT(type, field_name) visitor(#field_name, field_name);
 
-#define PP_IMPL_MEMBER(type_field_pair) __PP_EVAL(__PP_IMPL_MEMBER type_field_pair)
+#define PP_IMPL_MEMBER(cookie, type_field_pair) __PP_EVAL(__PP_IMPL_MEMBER type_field_pair)
 #define __PP_IMPL_MEMBER(type, field_name) type field_name;
 
 #define DEF_REFLECTABLE(name, ...)                              \
@@ -41,7 +41,7 @@
         static const char* get_name() { return #name; }         \
         template<typename visitor_t>                            \
         void foreach_member(visitor_t& visitor) const {         \
-            PP_FOR_EACH(PP_IMPL_VISIT, ##__VA_ARGS__)           \
+            PP_FOR_EACH(PP_IMPL_VISIT, _, ##__VA_ARGS__)        \
         }                                                       \
-        PP_FOR_EACH(PP_IMPL_MEMBER, ##__VA_ARGS__)              \
+        PP_FOR_EACH(PP_IMPL_MEMBER, _, ##__VA_ARGS__)           \
     }
